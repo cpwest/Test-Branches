00c73aaea9c55dcc95ada09efce565aec375e692
Catherine West <cat.p.west@gmail.com>
2018-07-09 20:12:09 -0400

    add another file

---
+++ helpers.py
@@ -0,0 +1,7 @@
+# Inside of `helpers.py`, write a function, called `greet`, 
+# that accepts a name and prints: `"Hello, <name>"`. 
+# For example, `greet("Jane")` would print: `"Hello, Jane"`.
+
+def greet():
+    name = input("What's your name?")
+    print("Hello" + str(name) +"!")
